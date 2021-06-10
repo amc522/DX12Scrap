bbec16b33d2e531ae6469631c56c583e00d27df6
amc522 <1531573+amc522@users.noreply.github.com>
2021-06-09 23:24:41 -0700

    Converting from a console application to a windows gui application.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include "Application.h"
+#include <Windows.h>
 
-int main(int argc, const char **argv)
+int WINAPI wWinMain(_In_ HINSTANCE /*hInstance*/,
+                   _In_opt_ HINSTANCE /*hPrevInstance*/,
+                   _In_ PWSTR /*lpCmdLine*/,
+                   _In_ int /*nCmdShow*/)
 {
     scrap::Application app;
     while(app)
