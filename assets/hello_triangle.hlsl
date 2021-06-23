b8d6abc2af2deca60782c13518b6ed0e8b584113
amc522 <1531573+amc522@users.noreply.github.com>
2021-06-23 00:22:48 -0700

    D3D12 texture part 8: Using the texture in the shader

--- assets/hello_triangle.hlsl
+++ assets/hello_triangle.hlsl
@@ -6,8 +6,12 @@ struct VertexInput
 struct VertexOutput
 {
     float4 clipPos : SV_Position;
+    float2 uv : TEXCOORD;
     float4 color : COLOR;
 };
+
+Texture2D<float> gTexture : register(t0);
+SamplerState gSampler : register(s0);
 
 VertexOutput VSMain(VertexInput input)
 {
@@ -17,18 +21,22 @@ VertexOutput VSMain(VertexInput input)
     {
     case 0:
         output.clipPos = float4(-0.5, -0.5, 0.0, 1.0);
+        output.uv = float2(0.0, 1.0);
         output.color = float4(1.0, 0.0, 0.0, 1.0);
         break;
     case 1:
         output.clipPos = float4(0.0, 0.5, 0.0, 1.0);
+        output.uv = float2(0.5, 0.0);
         output.color = float4(0.0, 1.0, 0.0, 1.0);
         break;
     case 2:
         output.clipPos = float4(0.5, -0.5, 0.0, 1.0);
+        output.uv = float2(1.0, 1.0);
         output.color = float4(0.0, 0.0, 1.0, 1.0);
         break;
     default:
         output.clipPos = 0.0.xxxx;
+        output.uv = 0.0.xx;
         output.color = float4(0.0.xxx, 1.0);
         break;
     }
@@ -38,5 +46,5 @@ VertexOutput VSMain(VertexInput input)
 
 float4 PSMain(VertexOutput input) : SV_Target
 {
-    return input.color;
+    return lerp(input.color, gTexture.Sample(gSampler, input.uv), 0.5);
 }
