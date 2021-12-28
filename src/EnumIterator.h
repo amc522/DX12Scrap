a3b26de1119031ce874369d2b285a390ec78ef09
amc522 <1531573+amc522@users.noreply.github.com>
2021-12-27 19:10:46 -0800

    Wanted to try playing around with concepts so added a concept to define enums with First, Last and Count values.

--- src/EnumIterator.h
+++ src/EnumIterator.h
@@ -91,12 +91,19 @@ private:
     std::underlying_type_t<EnumT> mIndex = 0;
 };
 
-template<class EnumT, EnumT First = EnumT::First, EnumT Last = EnumT::Last>
+template<class T>
+concept RangeEnum = std::is_enum_v<T> && requires
+{
+    T::First;
+    T::Last;
+    T::Count;
+    requires(ToUnderlying(T::Last) - ToUnderlying(T::First) + 1) == ToUnderlying(T::Count);
+};
+
+template<RangeEnum EnumT, EnumT First = EnumT::First, EnumT Last = EnumT::Last>
 class Enumerator
 {
 public:
-    static_assert(std::is_enum_v<EnumT>);
-
     [[nodiscard]] constexpr EnumIterator<EnumT> begin() const noexcept
     {
         return EnumIterator<EnumT>(ToUnderlying(First));
